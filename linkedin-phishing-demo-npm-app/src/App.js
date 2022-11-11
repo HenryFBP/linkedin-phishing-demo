6b7dc5b0da55a3be384013f53fe456ff4ff3639e
Henry Post <HenryFBP@gmail.com>
2022-11-10 20:47:10 -0500

    make phishy

--- linkedin-phishing-demo-npm-app/src/App.js
+++ linkedin-phishing-demo-npm-app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import LinkedIn from './LinkedIn_Logo.svg.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function nowYouFuckedUp() {
+    alert("Hey! This is NOT LinkedIn. \nThis is a fake website set up by Henry Post (http://henrypost.net/) to demonstrate \nhow incredibly easy it is to get 'phished'.")
+
+    alert("For more info, go to https://www.phishing.org/what-is-phishing ")
+}
+
+function App(props) {
+    return (<div className="Auth-form-container">
+        <form className="Auth-form" onSubmit={nowYouFuckedUp}>
+            <div className="Auth-form-content">
+
+                <div className={'linkedin-logo'}>
+                    <img src={LinkedIn}/>
+                </div>
+                <h3 className="Auth-form-title">Sign In</h3>
+                <p> Stay updated on your professional world </p>
+                <div className="form-group mt-3">
+                    <label>Email address</label>
+                    <br/>
+                    <input
+                        type="email"
+                        className="form-control mt-1"
+                        placeholder="Enter email"
+                    />
+                </div>
+                <br/>
+                <div className="form-group mt-3">
+                    <label>Password</label>
+                    <br/>
+                    <input
+                        type="password"
+                        className="form-control mt-1"
+                        placeholder="Enter password"
+                    />
+                </div>
+                <br/>
+                <div className="d-grid gap-2 mt-3">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+                <p className="forgot-password text-right mt-2">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </div>
+        </form>
+    </div>)
 }
 
 export default App;
